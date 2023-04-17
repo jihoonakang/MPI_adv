f0315abceee5129707696d76332ab99dda75527a
Kang, Ji-Hoon <jhkang@kisti.re.kr>
2023-04-17 19:19:16 +0900

    Create para_range.py

---
+++ mpi4py/para_range.py
@@ -0,0 +1,26 @@
+################################################################################
+# MPI python example
+#
+# Copyright 2023. Ji-Hoon Kang, All rights reserved.
+# This project is released under the terms of the MIT License (see LICENSE )
+################################################################################
+
+def para_range(n1, n2, size, rank) :
+    iwork = divmod((n2 - n1 + 1), size)
+    ista = rank * iwork[0] + n1 + min(rank, iwork[1])
+    iend = ista + iwork[0] - 1
+    if iwork[1] > rank :
+        iend = iend + 1
+
+    return ista, iend
+
+from mpi4py import MPI
+
+comm = MPI.COMM_WORLD
+
+size = comm.Get_size()
+rank = comm.Get_rank()
+
+ista, iend = para_range(10,102, size, rank)
+
+print(rank, ista, iend)
